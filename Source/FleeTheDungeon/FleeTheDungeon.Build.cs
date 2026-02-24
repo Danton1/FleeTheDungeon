0b02b0c7a3bb39011a805708297a3ae954a96a64
Danton Soares <dantonvsoares@gmail.com>
2026-02-23 20:48:02 -0800

    Setting up an Interact input action and refactoring the code, replacing old BP logic to C++

--- Source/FleeTheDungeon/FleeTheDungeon.Build.cs
+++ Source/FleeTheDungeon/FleeTheDungeon.Build.cs
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Copyright Epic Games, Inc. All Rights Reserved.
 
 using UnrealBuildTool;
 
@@ -7,14 +7,35 @@ public class FleeTheDungeon : ModuleRules
 	public FleeTheDungeon(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PublicDependencyModuleNames.AddRange(new string[] {
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"EnhancedInput",
+			"AIModule",
+			"StateTreeModule",
+			"GameplayStateTreeModule",
+			"UMG",
+			"Slate"
+		});
+
+		PrivateDependencyModuleNames.AddRange(new string[] { });
+
+		PublicIncludePaths.AddRange(new string[] {
+			"FleeTheDungeon",
+			"FleeTheDungeon/Variant_Horror",
+			"FleeTheDungeon/Variant_Horror/UI",
+			"FleeTheDungeon/Variant_Shooter",
+			"FleeTheDungeon/Variant_Shooter/AI",
+			"FleeTheDungeon/Variant_Shooter/UI",
+			"FleeTheDungeon/Variant_Shooter/Weapons"
+		});
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
+
 		// Uncomment if you are using online features
 		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
 
---
+++ Source/FleeTheDungeon/FleeTheDungeonGameMode.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "FleeTheDungeonGameMode.h"
+
+AFleeTheDungeonGameMode::AFleeTheDungeonGameMode()
+{
+	// stub
+}
